bf1b331071225ee327cd6b2069bea3e391563923
German Robayo Paz <germanrobayo33@gmail.com>
2017-07-29 23:52:16 -0400

    Finish the project

--- src/app.js
+++ src/app.js
@@ -51,21 +51,31 @@ const linearSearch = (item, arr) => {
   return false;
 }
 
-// Operators of the calculator
-const operators = ['plus', 'diff', 'mult', 'div'];
-const symbols = {
-  plus: '+',
-  diff: '-',
-  mult: 'x', // This will be mapped with a regex
-  div: '/'
+const fixNumber = (number, n) => {
+  /*
+    Description:
+      Function that fix the number of decimals in a number
+
+    Parameters:
+      number: number to chop
+      n: decimals quantity
+
+    Return:
+      The same number, but rounded up to n decimals
+  */
+  const m = Math.pow(10, n);
+  return Math.round(m * number) / m;
 }
+
+// Operators of the calculator
+const operators = ['+', '-', '×', '/'];
 // Get all buttons of the calculator
 const btns = _('.btn');
-
+const prevCalc = _id('prev-calc');
+const numberTyped = _id('number-typed');
 // Final operation
 let currNumber = "0";
-let prevNumber = "";
-let inOperation = false;
+let prevNumber = "0";
 
 /************************/
 /*      Main Script     */
@@ -75,65 +85,118 @@ let inOperation = false;
 btns.forEach(button => button.addEventListener('click', (e) => {
   
   const key = button.dataset.key;
-  
+  button.classList.add('active');
   // Check if it's a number.
-  if (parseInt(key) || key == 0) {
-    console.log(key, currNumber);
+  if (parseInt(key) || key == 0 || key === '.') {
     // Max number size
-    if (currNumber.length === 8) {
+    if (currNumber.length === 10) {
       return;
     }
     
-    // Check if it's empty and the user puts a 0. This is for avoid typing 0000...
-    if (!currNumber.length && key == 0) return;
+    // This is for avoid typing 0000...
+    if (currNumber === '0' && key == 0) return;
 
-    currNumber = currNumber === '0' 
+    // To avoid 1.111.11.
+    if (/\./.test(currNumber) && key === '.') return;
+
+    // If the key pressed was a . and the current number is a 0, just add it.
+    if (currNumber === '0' && key === '.') currNumber += '.';
+    else {
+      currNumber = currNumber === '0' 
                || ( (currNumber.length === 1) && linearSearch(currNumber, operators)) 
                ? key : currNumber + key;
-    _id('number-typed').innerText = currNumber;
+    }
+    if (numberTyped.className === "small" && currNumber.length < 9) {
+      numberTyped.classList.remove('small');
+    } else if (!numberTyped.className && currNumber.length >= 8) {
+      numberTyped.classList.add('small');
+    }
+    numberTyped.innerText = currNumber;
 
   } else { // It's a symbol
-    const lastLetter = currNumber[currNumber.length - 1];
-    console.log(key, lastLetter);
+
+    const lastLetter = prevNumber[prevNumber.length - 1];
+
     switch(key) {
-      case "plus":
-      case "div":
-      case "diff":
-      case "mult":
-        if (lastLetter === key) {
-          return;
-        } else if (!linearSearch(lastLetter, operators)) {
-          currNumber += key;
-          _id('prev-calc').innerText = currNumber;
-          _id('number-typed').innerText = key;
-          inOperation = true;
+      // These cases are straightforward
+      case "c": 
+        currNumber = "0";
+        numberTyped.innerText = currNumber;
+        if (numberTyped.className === "small") {
+          numberTyped.classList.remove("small");
         }
-        console.log('holi');
-      case "equal":
+        break;
+      case "ac":
+        if (numberTyped.className === "small") {
+          numberTyped.classList.remove("small");
+        }
+
+        currNumber = "0";
+        prevNumber = "0";
+        numberTyped.innerText = currNumber;
+        prevCalc.innerText = prevNumber;
+        break;
+
+      // Check for operation, all of them are the same case.
+      case "+":
+      case "/":
+      case "-":
+      case "×":
+        // If the user already typed an operator. TO FIX!!
         if (linearSearch(lastLetter, operators)) {
-          console.log('idc');
-        } else {
-          _id('prev-calc').innerText = currNumber;
-          _id('number-typed').innerText = "0";
+          if (currNumber === "0") return;
+          prevNumber = JSON.stringify(fixNumber(eval((prevNumber + currNumber).replace(/×/, "*")), 3));
+          prevCalc.innerText = prevNumber + key;
+          currNumber = "0";
+          numberTyped.innerText = prevNumber;
+          prevNumber += key;
+          return;
+        }
+
+        if (currNumber === "0" || currNumber === "0.") {
+          return;
+        }
+
+        prevNumber = currNumber + key;
+        currNumber = "0";
+        prevCalc.innerText = prevNumber;
+        numberTyped.innerText = currNumber;
+        break;
+
+
+      case "=":
+        // This is to avoid errors later and to show it better to the user.
+        if (lastLetter === '.') {
+          currNumber += "0";
+        }
+        // There's an operation in process.
+        if (linearSearch(lastLetter, operators)) {
+
+          /* The following line does this:
+            * replaces × for a *
+            * them eval() the expression
+            * finally just takes the 3 last decimal digits.
+          */
+          prevNumber = fixNumber(eval((prevNumber + currNumber).replace(/×/, "*")), 3);
+          prevCalc.innerText = prevNumber;
+          currNumber = "0";
+          numberTyped.innerText = prevNumber;
+
+        } else { // If it's just pressing = without making a real operation.
+
+          prevNumber = currNumber;
+          prevCalc.innerText = currNumber;
+          numberTyped.innerText = "0";
           currNumber = "0";
         }
+        break;
       default:
         break
     }
   }
 }));
 
-// // Let's listen for keydown events but just when they are numbers.
-// document.addEventListener('keydown', (e) => {
-//   if ((48 <= e.keyCode && e.keyCode <= 57) || (96 <= e.keyCode && e.keyCode <= 105)) {
-//     console.log(e.keyCode);
-//     // Max number size
-//     if (currNumber.length === 9) {
-//       return;
-//     }
-//     // Check if it's empty and the user puts a 0. This is for avoid typing 0000...
-//     if (!currNumber.length && e.key == 0) return;
-//     currNumber += e.key;
-//     _id('number-typed').innerText = currNumber;
-//   }
-// }); 
+btns.forEach(button => button.addEventListener('transitionend', (e) => {
+  if (e.propertyName !== "background-color") return;
+  button.classList.remove('active');
+}));
